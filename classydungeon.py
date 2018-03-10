9c216a983b8296e7ca5f36c625e26c45fa4fa003
fastfists <32852291+fastfists@users.noreply.github.com>
2018-03-09 18:01:09 -0600

    Made the update function operate faster

--- classydungeon.py
+++ classydungeon.py
@@ -6,16 +6,21 @@ class dungeon:
     notnull = []
     allrooms = []
     currenType = -1
-    def __init__(self ,resolution, roomCount):
-        self.prngNum = 406809004  #random.randint(0, 879190747)
+    def __init__(self ,resolution, roomCount, specialWeigth = 2):
+        self.prngNum = random.randint(0, 879190747)
         self.num = self.prngNum
         self.resolution = resolution
         self.roomCount = roomCount
         self.cantouch = [[0 for _ in range(self.resolution)] for _ in range(self.resolution) ]        
         self.Idtbl = [[0 for _ in range(self.resolution)] for _ in range(self.resolution)]
-
-    def _Prng(self,limit):
+        self.weight = specialWeigth
+    
+    def _Prng(self,limit, wantBool = False):
         self.prngNum = (self.prngNum * 154687469+879190747) % 67280421310721
+        if wantBool:
+            return self.prngNum % limit == 0
+        else:
+            pass
         return self.prngNum % limit
 
     def __repr__(self):
@@ -34,22 +39,34 @@ class dungeon:
             newX , newY = x + moveX , y + moveY
             try:
                 if self.Idtbl[newX][newY] == 0 and self.cantouch[x][y] == 0:
-                    if bool(self._Prng(1)):
+                    if self._Prng(self.weight,wantBool=True):
+                        print("boom")
                         size = (4,2)
-                    if self.Idtbl[x][y] == 2:
+                        self.weight *= 8
+                    elif self.Idtbl[x][y] == 2:
                         size = 4
                     elif self.Idtbl[x][y] == 3 or self.Idtbl[x][y] == 4:
                         size = 2
                     if self._format(size, newX,newY):
                         self.allrooms.append(Room(size, self.currenType,newX, newY , self.notnull, self.Idtbl))
-                        self.update()
+                        newX , newY = self.__startVal(self.allrooms[ len(self.allrooms) - 1 ])
+                        self.update(size, self.currenType,newX, newY)
                         # dont forget doors.
             except:
                 continue
+        self.addWalls()
     
+    def __startVal(self, room):
+        startVal = 0
+        for section in room.blocks:
+            for x,y in section:
+                if startVal > x + y:
+                    startVal = x + y
+        return startVal
+
     def draw(self):
         master = Tk()
-        dungeon= Canvas(master,width=1000,height=1000)
+        dungeon= Canvas(master,width=700,height=700)
         for y in range(self.resolution):
             for x in range (self.resolution):
                 w=1
@@ -57,8 +74,8 @@ class dungeon:
                 try:
                     ide = self.Idtbl[x][y] % 5
                 except:
-                    print("unique")
-                    ide = 4
+                    self.Idtbl[x][y] = 5
+                    ide = 5
                 if ide == 0:
                     f= 'gray'
                 elif ide == 1:
@@ -69,7 +86,9 @@ class dungeon:
                     f= 'green'
                 elif ide == 4:
                     f='purple'
-                if self.Idtbl[x][y] > 4:
+                elif ide ==5:
+                    f= 'red'
+                if self.Idtbl[x][y] > 5:
                     o='brown'
                     w=1
                 dungeon.create_rectangle(x*(700//self.resolution),y*(700//self.resolution),((x+1)*(700//self.resolution))-1,((y+1)*(700//self.resolution))-1,fill=f,outline=o,width=w) 
@@ -113,9 +132,10 @@ class dungeon:
                 return works
         return False  # returns False because none of the types work
 
-    def update(self):
-        for y in range(self.resolution):
-            for x in range(self.resolution):
+    def update(self, size, how, startX, startY):
+        #TODO change this update function to be mroe "limited"
+        for y in range(startY + 2):
+            for x in range(startX + 2):
                 _pass = True
                 for d in range(4):
                     dirX, dirY = self.__findDir(d)
@@ -155,7 +175,26 @@ class dungeon:
             return (1,1)
     
     def doors(self):
+        #TODO add this later in a better way(mabey as a Room method)
         pass
+    
+    def addWalls(self):
+        for y in range(self.resolution):
+            for x in range(self.resolution):
+                if (x,y) in self.notnull:
+                    for direction in range(8):
+                        nx,ny = self.__findDir(direction)
+                        neighborX, neighborY = nx+x, ny+y
+                        try:
+                            if self.Idtbl[neighborX][neighborY] == 0 and neighborX >= 0 and neighborY >= 0:
+                                self.Idtbl[neighborX][neighborY] = 1 
+                        except:
+                            pass
+
+    def removeWalls(self):
+        for y in range (self.resolution):
+            for x in range (self.resolution):
+                if self.Idtbl[x][y] == 1: self.Idtbl[x][y] == 0
 
 class Room:
     def __init__(self, size, how, sx,sy, notnull, idtbl):
@@ -187,5 +226,5 @@ if __name__ == "__main__":
     d = dungeon(20,20)
     d.make()
     d.draw()
-
-    print(" Rand int:",d.num)
+    for i in range(10):
+        print(bool(d._Prng(2)))
