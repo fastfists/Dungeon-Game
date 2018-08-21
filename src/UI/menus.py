d379dbaea321c405c5b44f42b3ea508b60834d35
DENZELL FORD - 474932 <474932@GPISD.ORG>
2018-08-21 13:48:35 -0500

    Added a centering for the menus

--- src/UI/menus.py
+++ src/UI/menus.py
@@ -1,12 +1,14 @@
 from . import TextButton
 import pygame
+import numpy as np
+from . import utils
 
 
 class PauseMenu(pygame.sprite.Group):
     font_size = 32
 
     def __init__(self, screen_size:tuple, *a, **kw):
-        WIDTH, HEIGHT = screen_size
+        self.width, self.height = WIDTH, HEIGHT = screen_size
         font_size = 32
 
         Gap = MenuCentered(screen_size, font_size, 4)
@@ -14,8 +16,11 @@ class PauseMenu(pygame.sprite.Group):
         self.options_button = TextButton(Gap(), "Options")
         self.restart_button = TextButton(Gap(), "Restart")
         self.quit_button = TextButton(Gap(), "Quit")
-        super().__init__(*self.__dict__.values())
-
+        super().__init__(self.resume_button, self.options_button, self.restart_button, self.quit_button)
+    
+    def draw(self, suface):
+        super().draw(suface)
+        pygame.draw.aaline(suface, utils.GRAY, (0, self.height//2), (self.width, self.height//2))
 
 class EndGameMenu(pygame.sprite.Group):
 
@@ -29,26 +34,34 @@ class EndGameMenu(pygame.sprite.Group):
 
 class MenuCentered:
     """
-    Generator class that provides x and why
+    Generator class that provides x and y
+    coordinates for 
     """
     def __init__(self, screen_size: tuple, gap_size, item_count:int):
         self.item_count = item_count
         self.WIDTH, self.HEIGHT = screen_size
         self.gap_size = gap_size
         self.current = 0
+        self.gen = iter(self.modify())
+
+    def modify(self):
+        skip_0 = True if self.item_count % 2 == 0 else False
+        for i in np.arange((self.item_count- 1) * -0.5, self.item_count):
+            if i == 0 and skip_0:
+                continue
+            yield i
+        raise StopIteration
 
     def __call__(self):
         return next(self)
 
     def __iter__(self):
-        self.current = 0
         return self
-    
+
     def __next__(self):
         if self.current < self.item_count:
             x = self.WIDTH // 2
-            y = (self.HEIGHT // 2) + (self.gap_size * self.current)
-            print(self.current)
+            y = (self.HEIGHT // 2) + (self.gap_size * next(self.gen))
             self.current += 1
-            return x,y
+            return x, y
         raise StopIteration
